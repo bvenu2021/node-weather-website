9018b2878bb0f1ca17c135560b0bf669d7cd28bd
Venugopal Bellary <bvenu2001@gmail.com>
2020-07-15 21:12:11 +0530

    Added humidity in the forecase report

--- src/utls/forecast.js
+++ src/utls/forecast.js
@@ -9,7 +9,7 @@ module.exports = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback("Unable to find the location! Try another search", undefined)
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + " degrees out, and it feels like " + body.current.feelslike + " degrees out")
+            callback(undefined, body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + " degrees out, and it feels like " + body.current.feelslike + " degrees out. Humidity is " + body.current.humidity)
         }
     })
 }
